2e067c6f9a515b6b90f397b0f799063553aeb555
Yakub Mohammad <arusatechnology@gmail.com>
2026-02-18 15:09:15 -0800

    ADAS-54: Adding support to capacitor-spm 8.0.0

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "LlamaCpp",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
-            name: "LlamaCpp",
+            name: "LlamaCppCapacitor",
             targets: ["LlamaCppPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
     ],
     targets: [
         .target(
